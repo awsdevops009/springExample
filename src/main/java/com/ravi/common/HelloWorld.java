e2f14ad99fdee75b456595512b6b4c1c8de8a1a1
pradeep.dash <awsdevops009@gmail.com>
2020-01-05 23:13:34 +0530

    file added

--- src/main/java/com/ravi/common/HelloWorld.java
+++ src/main/java/com/ravi/common/HelloWorld.java
@@ -13,6 +13,6 @@ public class HelloWorld
 	}
 	
 	public void printHello() {
-		System.out.println("Hello How are tomorrow   " + name);
+		System.out.println("Hello How are yesterday   " + name);
 	}
 }
